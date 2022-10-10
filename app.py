98598b929d24e89aa534fcf52086d59ccfc930c0
Nazia Siddique <nazia.siddique04@gmail.com>
2022-10-10 17:50:28 +0000

    app.py updated with amended code

--- app.py
+++ app.py
@@ -1,7 +1,37 @@
 """ imports """
-from twister import app
-
+from http import client
+from twister import app 
+import pymongo
+from flask import Flask
+from pymongo import MongoClient
+# from twister.create_db import *
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    app.run(host='127.0.0.1', port=5000)
+    # print('welcome to mongodb')
+    # print(client)
+    app.run(host='0.0.0.0', port=5000,debug=True)
+   
+
+# app = Flask(__name__)
+
+# client = MongoClient('localhost', 27017)
+
+# db = client.flask_db
+# todos = db.todos
+
+
+# how to connect multiple databases in flask ?
+# SQLALCHEMY_DATABASE_URI = 'postgres://localhost/main'
+# SQLALCHEMY_BINDS = {
+#     'users':        'mysqldb://localhost/users',
+#     'appmeta':      'sqlite:////path/to/appmeta.db'
+# }
+
+
+
+
+
+
+
+
+
